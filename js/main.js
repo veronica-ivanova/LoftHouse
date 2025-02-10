432ec071c32a6eeb2d2891406958cba2d72c6d5f
veronica-ivanova <veronica.ivanova27@gmail.com>
2025-02-10 21:13:55 +0300

    add phoneMask and youtubeLightbox

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ navBtn.onclick = function () {
     nav.classList.toggle('header__top-row--mobile');
     document.body.classList.toggle('no-scroll');
 }
+
+// Phone mask
+mask('[data-tel-input]');
+
+// Удаляем '+' если больше ничего не введено, чтобы показать placeholder
+const phoneInputs = document.querySelectorAll('[data-tel-input]');
+phoneInputs.forEach((input)=>{
+	input.addEventListener('input', ()=>{
+		if (input.value == '+') input.value = '';
+	})
+	input.addEventListener('blur', ()=>{
+		if (input.value == '+') input.value = '';
+	})
+});
